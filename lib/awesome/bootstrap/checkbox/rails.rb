cb65d60caa3b0e3d58e4c9c60f5744cc3e59f43f
Toshiyuki HINA <toshiyuki.hina@gmail.com>
2018-10-11 08:53:19 +0900

    initial commit

---
+++ lib/awesome/bootstrap/checkbox/rails.rb
@@ -0,0 +1,12 @@
+require "awesome/bootstrap/checkbox/rails/version"
+
+module Awesome
+  module Bootstrap
+    module Checkbox
+      module Rails
+        class Engine < ::Rails::Engine
+        end
+      end
+    end
+  end
+end
---
+++ lib/awesome/bootstrap/checkbox/rails/version.rb
@@ -0,0 +1,9 @@
+module Awesome
+  module Bootstrap
+    module Checkbox
+      module Rails
+        VERSION = "0.3.7"
+      end
+    end
+  end
+end
